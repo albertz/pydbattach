ed2d5ba84bedb75ba5133c99b887005bfbdfe773
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-05-26 06:49:29 +0200

    giving instructions

--- demo_pyinjectcode.sh
+++ demo_pyinjectcode.sh
@@ -4,8 +4,15 @@ set -o monitor
 ./dummy-python-proc.py &
 pypid=$!
 
+dylib="pyinjectcode.dylib"
+[ \! -e "$dylib" ] && dylib="pyinjectcode.so"
+[ \! -e "$dylib" ] && dylib="pyinjectcode.dll"
+[ \! -e "$dylib" ] && \
+	echo "pyinjectcode.{dylib,so,dll} not found." && \
+	echo "compile the library first." && exit 1
+
 {
-	echo 'set $m = (void*)dlopen("./pyinjectcode.dylib", 8)'
+	echo "set \$m = (void*)dlopen(\"$dylib\", 8)"
 	echo 'set $f = (void*)dlsym($m, "inject")'
 	echo 'call (int)$f()'
 	echo 'quit'
