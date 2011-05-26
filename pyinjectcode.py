37355b1941fc8f446597e2a63626dd02b857b7d6
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-05-26 19:41:03 +0200

    be sure that we have the correct path in sys.path

--- pyinjectcode.py
+++ pyinjectcode.py
@@ -1,15 +1,16 @@
-print "Hello from pyinjectcode"
+print "Hello from pyinjectcode:", __file__
 
-import pythreadhacks
+import os, os.path, sys
+sys.path += [os.path.dirname(__file__)]
 
 import sys, thread
 threads = [t for t in sys._current_frames().keys() if t != thread.get_ident()]
 
 print "pyinjectcode found threads:", threads
-
 assert threads, "fatal, no threads found"
 
 print "attaching to first thread ..."
 tid = threads[0]
-pythreadhacks.pdbIntoRunningThread(tid)
 
+import pythreadhacks
+pythreadhacks.pdbIntoRunningThread(tid)
