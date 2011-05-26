7a2d1fdbe2f36ae6339e10fcd6840b57e6f36bf3
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-05-26 06:54:54 +0200

    small description

--- demo_pyinjectcode.sh
+++ demo_pyinjectcode.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# This demo scripts starts some dummy Python process (dummy-python-proc.py).
+#
+# Then, via GDB, it dynamically loads a previously prepared library (pyinjectcode.c)
+# and calls the function 'inject()' from it.
+#
+# The library pyinjectcode.c creates a new Python thread and runs
+# the code from pyinjectcode.py.
+#
+# The Python code pyinjectcode.py uses some magic in pythreadhacks.py
+# to attach with Pdb to the first other Python thread.
+
 set -o monitor
 ./dummy-python-proc.py &
 pypid=$!
