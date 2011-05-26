6ff540cab64c1a907a739362e9d61bba95f85a54
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-05-26 06:45:08 +0200

    pythreadhacks.pdbIntoRunningThread for first thread

--- pyinjectcode.py
+++ pyinjectcode.py
@@ -1,1 +1,15 @@
 print "Hello World from pyinjectcode"
+
+import pythreadhacks
+
+import sys, thread
+threads = [t for t in sys._current_frames().keys() if t != thread.get_ident()]
+
+print "pyinjectcode found threads:", threads
+
+assert threads, "fatal, no threads found"
+
+print "attaching to first thread ..."
+tid = threads[0]
+pythreadhacks.pdbIntoRunningThread(tid)
+
